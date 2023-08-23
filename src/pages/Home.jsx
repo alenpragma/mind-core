248b9bd177ce5bf1bcd26a30c81ab8bb246b6fd2
sajid <sajid.mern2201@gmail.com>
2023-08-23 16:53:21 +0600

    added conainer

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RightSidebar from "../components/home/RightSidebar"
+
+
+const Home = () => {
+  return (
+    <>
+      <RightSidebar />
+    </>
+  )
+}
+
+export default Home
