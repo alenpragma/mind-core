6bf6ba256a1eeb3d7452c4cb96a522a64cfef643
sajid <sajid.mern2201@gmail.com>
2023-08-23 17:50:20 +0600

    modified folder structure

--- src/components/Container.jsx
+++
@@ -1,9 +0,0 @@
-import React { children } from 'react'
-
-const Container = ({children}) => {
-  return (
-    <div className='container mx-auto'>{children}</div>
-  )
-}
-
-export default Container
