41cc375a6c6e3ef05f83131f6ce47c020eaa3262
sajid <sajid.mern2201@gmail.com>
2023-08-24 17:52:39 +0600

    aligned right sidebar bottom left

--- src/components/page/home/RightSidebar/RightSidebar.jsx
+++ src/components/page/home/RightSidebar/RightSidebar.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {BsFillSendFill} from "react-icons/bs"
 import {BsQrCodeScan} from "react-icons/bs"
 import {FaFingerprint} from "react-icons/fa"
 
 const RightSidebar = () => {
+  
+  const [toggleSide, setToggleSide] = useState(false)
+
   return (
-    <aside className='relative'>
-        <div className="absolute top-[20px] right-[20px] w-[5%]">
+    <aside className=''>
+        <div className={`absolute bottom-[20%] right-[20px] hello w-[110px] rounded glass-container ${toggleSide ? "w-[150px]" : ""}`}>
             <div className="right-sidebar text-white w-[100%] ">
-                <div className="right-sidebar__item flex flex-col gap-[20px] p-[20px] rounded bg-[#1A1A1C]">
+                <div className="right-sidebar__item flex flex-col gap-[15px] py-[20px] px-[30px] ">
                   <div className='text-center flex flex-col items-center'>
-                      <div className=" bg-[#3C3C3E] relative w-[50px] h-[50px] rounded-full">
-                      <BsFillSendFill className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[30px]' />
+                      <span className=" bg-[#fff3] relative w-[50px] h-[50px] rounded-full cursor-pointer flex justify-center items-center">
+                      <BsFillSendFill className=' text-[25px] flex justify-center' />
+                      </span>
+                    <p className='text-[20px] pt-[10px]'>Send</p>
+                  </div>
+                  <div className='text-center flex flex-col items-center '>
+                      <div className=" bg-[#fff3] relative w-[50px] h-[50px] rounded-full cursor-pointer flex justify-center items-center">
+                      <BsQrCodeScan className=' text-[25px]' />
                       </div>
-                    Send
+                      <p className='text-[20px] pt-[10px]'>Receive</p>
                   </div>
                   <div className='text-center flex flex-col items-center'>
-                      <div className=" bg-[#3C3C3E] relative w-[50px] h-[50px] rounded-full">
-                      <BsQrCodeScan className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[30px]' />
+                      <div className=" bg-[#fff3] relative w-[50px] h-[50px] rounded-full cursor-pointer flex justify-center items-center">
+                      <FaFingerprint className=' text-[25px]' />
                       </div>
-                      Receive
-                  </div>
-                  <div className='text-center flex flex-col items-center'>
-                      <div className=" bg-[#3C3C3E] relative w-[50px] h-[50px] rounded-full">
-                      <FaFingerprint className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[30px]' />
-                      </div>
-                      Bridge
+                      <p className='text-[20px] pt-[10px]'>Bridge</p>
                   </div>
                  
                 </div>
