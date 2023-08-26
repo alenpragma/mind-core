c82015d671f5f1703eae0734c99242cba882d7e2
HamzaWebPro <hamza.mern2201@gmail.com>
2023-08-26 10:31:53 +0600

    right sidebar toole added

--- src/components/page/home/RightSidebar/RightSidebar.jsx
+++ src/components/page/home/RightSidebar/RightSidebar.jsx
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom'
 import {BsFillSendFill} from "react-icons/bs"
 import {BsQrCodeScan} from "react-icons/bs"
 import {FaFingerprint} from "react-icons/fa"
+ import{PiCurrencyBtcBold} from "react-icons/pi"
 
 const RightSidebar = () => {
-  
+  let [show,setShow] = useState(false)
 
   return (
     <aside className=''>
-        <div className="fixed z-10 bottom-4 right-4 hello rounded glass-container">
+      {/* transact open button start*/}
+      <div onClick={()=>setShow((prev)=>!prev)} className="fixed bottom-[10px] right-4 text-white flex items-center gap-x-1 text-[20px] font-semibold p-2 rounded-lg glass-container duration-100 group  cursor-pointer"><PiCurrencyBtcBold className='text-[25px] group-hover:text-colorprimary duration-100'/><span className='group-hover:text-colorprimary duration-100'>Transact</span> </div>
+      {/* transact open button end */}
+        <div className={`fixed z-10 bottom-[70px]  ${show ? "right-4":"right-[-150px]"} duration-300   hello rounded glass-container`}>
             <div className="right-sidebar text-white w-[100%] ">
                 <div className="right-sidebar__item flex flex-col gap-[15px] py-[15px] px-[10px] ">
                   <div className='text-center flex flex-col items-center hover:text-colorprimary duration-100'>
